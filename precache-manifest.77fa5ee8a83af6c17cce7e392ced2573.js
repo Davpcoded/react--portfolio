51fd6d7a679ce1caafeb6d3dadcb9fec4493a134
Daniel Villarroel-Posada <danielv@friends.local>
2020-08-11 08:47:47 -0700

    Updates

---
+++ precache-manifest.77fa5ee8a83af6c17cce7e392ced2573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9656335d31033b2c8cbd3ace06ea842",
+    "url": "/react--portfolio/index.html"
+  },
+  {
+    "revision": "1aa0a21bfd6ccd452ba1",
+    "url": "/react--portfolio/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "4081b8c866b2527d1304",
+    "url": "/react--portfolio/static/css/main.8fd01711.chunk.css"
+  },
+  {
+    "revision": "1aa0a21bfd6ccd452ba1",
+    "url": "/react--portfolio/static/js/2.0b26c76e.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/react--portfolio/static/js/2.0b26c76e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4081b8c866b2527d1304",
+    "url": "/react--portfolio/static/js/main.59d86bc0.chunk.js"
+  },
+  {
+    "revision": "0ed42dd875c9da1837ce",
+    "url": "/react--portfolio/static/js/runtime-main.10e4cb6a.js"
+  }
+]);
